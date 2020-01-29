3c056a84abd2367e02882b758a004a14ed791264
Tien Nguyen <tnguyen0393@gmail.com>
2020-01-29 16:42:22 -0600

    Updates

---
+++ precache-manifest.b7462a74036901f5bb213297b31d72f3.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c057aa489686c4080df0892688a37615",
+    "url": "/NucampReactRedux/index.html"
+  },
+  {
+    "revision": "c759679ee1c48f7d8d36",
+    "url": "/NucampReactRedux/static/css/2.c8cff95b.chunk.css"
+  },
+  {
+    "revision": "2dab16f0ca3e4349c0ad",
+    "url": "/NucampReactRedux/static/css/main.2529afa6.chunk.css"
+  },
+  {
+    "revision": "c759679ee1c48f7d8d36",
+    "url": "/NucampReactRedux/static/js/2.3d4ae404.chunk.js"
+  },
+  {
+    "revision": "7478ab18f99d6ab577f1b4d3d256caec",
+    "url": "/NucampReactRedux/static/js/2.3d4ae404.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2dab16f0ca3e4349c0ad",
+    "url": "/NucampReactRedux/static/js/main.955a18ba.chunk.js"
+  },
+  {
+    "revision": "5decbeec7445aa843d22",
+    "url": "/NucampReactRedux/static/js/runtime-main.e6a53e33.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/NucampReactRedux/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/NucampReactRedux/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/NucampReactRedux/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/NucampReactRedux/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/NucampReactRedux/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "02bba601716f9a0ff1e241a64cfe9b29",
+    "url": "/NucampReactRedux/static/media/lobster-latin-400.02bba601.woff"
+  },
+  {
+    "revision": "fe012fb7b22e5776c4a2e91889068974",
+    "url": "/NucampReactRedux/static/media/lobster-latin-400.fe012fb7.woff2"
+  },
+  {
+    "revision": "24f7b0944e9e03a905f9d7701573b2cd",
+    "url": "/NucampReactRedux/static/media/open-sans-latin-300.24f7b094.woff2"
+  },
+  {
+    "revision": "a7622f60c56ddd5301549a786b54e6e6",
+    "url": "/NucampReactRedux/static/media/open-sans-latin-300.a7622f60.woff"
+  },
+  {
+    "revision": "6725c3fb1f3abd7ebdcb2d244758f706",
+    "url": "/NucampReactRedux/static/media/open-sans-latin-300italic.6725c3fb.woff2"
+  },
+  {
+    "revision": "793b1237017aeacd646fb80911425566",
+    "url": "/NucampReactRedux/static/media/open-sans-latin-300italic.793b1237.woff"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "/NucampReactRedux/static/media/open-sans-latin-400.33543c5c.woff2"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/NucampReactRedux/static/media/open-sans-latin-400.de0869e3.woff"
+  },
+  {
+    "revision": "06b4bfda4e139eaf3ab9872a6d66f42f",
+    "url": "/NucampReactRedux/static/media/open-sans-latin-400italic.06b4bfda.woff"
+  },
+  {
+    "revision": "b61a9055d92ac56c15cacf4271d4f072",
+    "url": "/NucampReactRedux/static/media/open-sans-latin-400italic.b61a9055.woff2"
+  },
+  {
+    "revision": "449d681cd6006390e1bee3c3a660430b",
+    "url": "/NucampReactRedux/static/media/open-sans-latin-600.449d681c.woff"
+  },
+  {
+    "revision": "819af3d3abdc9f135d49b80a91e2ff4c",
+    "url": "/NucampReactRedux/static/media/open-sans-latin-600.819af3d3.woff2"
+  },
+  {
+    "revision": "56e5756b696615d6164a625e1bcb1a9e",
+    "url": "/NucampReactRedux/static/media/open-sans-latin-600italic.56e5756b.woff"
+  },
+  {
+    "revision": "ebdfa24cb411ba3016abd1c3ea5746e5",
+    "url": "/NucampReactRedux/static/media/open-sans-latin-600italic.ebdfa24c.woff2"
+  },
+  {
+    "revision": "0edb76284a7a0f8db4665b560ee2b48f",
+    "url": "/NucampReactRedux/static/media/open-sans-latin-700.0edb7628.woff2"
+  },
+  {
+    "revision": "1f85e92d8ff443980bc0f83ad7b23b60",
+    "url": "/NucampReactRedux/static/media/open-sans-latin-700.1f85e92d.woff"
+  },
+  {
+    "revision": "a49393f2171a2f4d6927ce6c14f73dbc",
+    "url": "/NucampReactRedux/static/media/open-sans-latin-700italic.a49393f2.woff2"
+  },
+  {
+    "revision": "bf72679ca22e53320beaea090e8bb07d",
+    "url": "/NucampReactRedux/static/media/open-sans-latin-700italic.bf72679c.woff"
+  },
+  {
+    "revision": "05ebdbe10796850f045fcd484f35788d",
+    "url": "/NucampReactRedux/static/media/open-sans-latin-800.05ebdbe1.woff"
+  },
+  {
+    "revision": "ab3e500e7375695d702cff19513e4470",
+    "url": "/NucampReactRedux/static/media/open-sans-latin-800.ab3e500e.woff2"
+  },
+  {
+    "revision": "92da6f116d973bd334cf9b3afdb29c4f",
+    "url": "/NucampReactRedux/static/media/open-sans-latin-800italic.92da6f11.woff"
+  },
+  {
+    "revision": "c62c3eea77cf3fca51494e8c924adf4d",
+    "url": "/NucampReactRedux/static/media/open-sans-latin-800italic.c62c3eea.woff2"
+  }
+]);
